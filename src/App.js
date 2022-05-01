bc30993caa37f7f265d5b33702e05989216fcd62
ah-ryeong <aryeong22@hanmail.net>
2022-05-01 20:06:59 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js';
 
 function App() {
+
+  let [book] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar bg="light" variant="light">
+        <Container>
+        <Navbar.Brand href="#home">Winter BookStore</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Cart</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+
+      <div className = "main-bg"></div>
+
+      <div className = "container">
+        <div className="row">
+          
+          {
+            book.map((a, i) => {
+              return <Card book = {book[i]} i ={i}></Card>
+            })
+          }
+
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+function Card(props) {
+  return(
+    <div>
+      <div className="col-md-4">
+          <img src = {props.book.img} height ="300px" />
+          <h4>{props.book.title}</h4>
+          <p>{props.book.price}</p>
+        </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,27 @@
+let data = [
+
+    {
+        id: 0,
+        img : "http://image.kyobobook.co.kr/images/book/large/150/l9791157061150.jpg",
+        title : "고기를 끊지 못 하는 사람들",
+        content : "인류의 육식 연대기",
+        price : "15,300"
+    },
+    {
+        id: 1,
+        img : "http://image.kyobobook.co.kr/images/book/large/447/l9791186602447.jpg",
+        title : "아무튼, 비건",
+        content : "당신도 연결되어 있나요?",
+        price : "10,800"
+    },
+    {
+        id: 2,
+        img : "http://image.kyobobook.co.kr/images/book/large/778/l9788936486778.jpg",
+        title : "돼지를 키운 채식주의자",
+        content : "마당의 돼지들에게 배운 동물을 키우고 먹는다는 것의 의미",
+        price : "13,500"
+    },
+
+];
+
+export default data;
